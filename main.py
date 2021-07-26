3ed0690f215eb47c6ce5b297a00b38b6416fc69e
Benjamin-H1 <hypnos11hypnos@gmail.com>
2021-07-26 11:40:22 +0100

    Created a blueprint for main.py to define future workflow

---
+++ main.py
@@ -0,0 +1,22 @@
+def download(files, conn):
+    #submit files for download
+    pass
+
+def connect(address, port, login, password):
+    #establishes connection with server
+    pass
+
+def gui():
+    #runs the program with the graphical interface
+    pass
+
+if __name__ == '__main__':
+    #Check if run as a GUI or not
+    #if GUI, run gui()
+    #else check the arguments and extract relevant information such as:
+        #IP address
+        #port
+        #login
+        #password
+        #date/time for scheduled download
+        #date/time of data to be retrieved
