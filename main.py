ff35679438fd7bfdca06c8a5930725f5218631e0
Benjamin-H1 <hypnos11hypnos@gmail.com>
2021-07-26 12:11:04 +0100

    Added to download procedure to download files from server

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import ftplib
 
 def download(files, conn):
-    #submit files for download
-    pass
+    for filename in files:
+        with open(filename, "wb") as file:
+            conn.retrbinary(f"RETR {filename}", file.write)
 
 def connect(address, port, login, password):
     #establishes connection with server
@@ -28,8 +29,9 @@ if __name__ == '__main__':
         #date/time for scheduled download
         #date/time of data to be retrieved
     #for now I am going to rather irresponsibly hard code the login details
-    conn = connect("87.115.189.50", "21", "roadrunner", "M33pM33p")
+    conn = connect("87.115.189.50", "21", "Roadrunner", "M33pM33p")
     if conn == False:
         print("Error connecting to server")
         exit()
     print("Connection successful!!")
+    conn.quit()
